29fd8db9d7d5b6f70cbd255ea3e74e1bd2192534
goyal-nandini <nandinigoyal1981@gmail.com>
2025-12-08 12:23:17 +0530

    Update code logic

--- Blackjack-21/Blackjack.py
+++ Blackjack-21/Blackjack.py
@@ -1,4 +1,4 @@
-# to do now :)
+# Mine code
 import random
 from art import logo
 import os
@@ -6,6 +6,7 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 want_continue = True
 
 def calculate_score(cand_cards):
+    """Take a list of cards and return its sums"""
     score = sum(cand_cards)
     if score > 21 and 11 in cand_cards:
         cand_cards.remove(11)
@@ -13,15 +14,11 @@ def calculate_score(cand_cards):
     return score
 
 def asking_user_choice():
+    """Asks user their choice"""
     user_choice = input("Type 'y' to get another card, type 'n' to pass: ").lower()
     if user_choice == 'y':
         c3 = random.choice(cards)
         user_cards.append(c3)
-        # user_cards_sum = sum(user_cards)
-        # if user_cards_sum > 21 and c3 == 11:
-        #     user_cards.remove(c3)
-        #     c3 = 1
-        #     user_cards.append(c3)
         print(f"Your cards: {user_cards}, final score: {calculate_score(user_cards)}")
         if calculate_score(user_cards) > 21:
             print(f"Computer's final hand: {comp_cards}, final score: {calculate_score(comp_cards)}")
@@ -31,10 +28,6 @@ def asking_user_choice():
         else:
             asking_user_choice()
     else:
-        # print(f"Your final hand: {user_cards}, final score: {calculate_score(user_cards)}")
-        # # cc2 = random.choice(cards)
-        # # comp_cards.append(cc2)
-        # print(f"Computer's final hand: {comp_cards}, final score: {calculate_score(comp_cards)}")
         print(f"Your final hand: {user_cards}, final score: {calculate_score(user_cards)}")
 
         # Dealer draws until reaching 17
@@ -43,8 +36,6 @@ def asking_user_choice():
 
         print(f"Computer's final hand: {comp_cards}, final score: {calculate_score(comp_cards)}")
 
-        # comp_cards_sum = sum(comp_cards)
-        # user_cards_sum = sum(user_cards)
         if calculate_score(comp_cards) > 21:
             print("Opponent went over. You win!")
         elif calculate_score(comp_cards) == 21:
@@ -53,14 +44,6 @@ def asking_user_choice():
             print("Its draw")
         elif calculate_score(comp_cards) > calculate_score(user_cards):
             print("You lose")
-        # elif calculate_score(comp_cards) < calculate_score(user_cards):
-        #     print("You win!")
-        # elif comp_cards_sum == user_cards_sum:
-        #     print("Its draw")
-        # elif comp_cards_sum > user_cards_sum:
-        #     print("You lose")
-        # elif comp_cards_sum < user_cards_sum:
-        #     print("You win")
         else:
             asking_user_choice()
 
@@ -73,7 +56,7 @@ while want_continue:
     else:
         break
 
-    # have to give 2 cards to user 
+    # give 2 cards to user 
     user_cards = []
     comp_cards = []
     c1 = random.choice(cards)
@@ -82,7 +65,7 @@ while want_continue:
     user_cards.append(c2)
     print(f"Your cards: {user_cards}, current score: {calculate_score(user_cards)}")
 
-    # one card for computer
+    # one card for computer to show to user
     cc1 = random.choice(cards)
     cc2 = random.choice(cards) # not shown to user
     comp_cards.append(cc1)
@@ -110,20 +93,3 @@ while want_continue:
 
     # asking user
     asking_user_choice()
-    # user_choice = input("Type 'y' to get another card, type 'n' to pass:").lower()
-    # if user_choice == 'y':
-    #     c3 = random.choice(cards)
-    #     user_cards.append(c3)
-    #     user_cards_sum = sum(user_cards)
-    #     print(f"Your cards: {user_cards}, final score: {sum(user_cards)}")
-    #     if user_cards_sum > 21:
-    #         print(f"Computer's final hand: {comp_cards}, final score: {sum(comp_cards)}")
-    #         print("You lose")
-    #     else:
-
-
-    # cc2 = random.choice(cards)
-    # comp_cards.append(cc2)
-    # print(f"Computer's cards: {comp_cards}")
-    # comp_cards_sum = sum(comp_cards)
-
