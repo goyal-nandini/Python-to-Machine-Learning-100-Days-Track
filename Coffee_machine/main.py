9052faa9e38400888cce0d1cc4488a9b422a4a28
Nandini <nandinigoyal1981@gmail.com>
2025-12-15 23:59:34 +0530

    Improve coffee machine main loop logic
    Refactor main loop for better flow and error handling.

--- Coffee_machine/main.py
+++ Coffee_machine/main.py
@@ -1,35 +1,35 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# lets do this problem then we have to cover day17 also today only...as 19th day
-# hey make the object name good not this bad obj1 obj2 eww...
-# bugs hai so logical okay hai but program flow error hai -_- but mam wala ek dum same hai so its for yr understanding the correct flow!!
-# leaving as it is seems okay for now rest i am checking q&a
-menu = Menu()
-coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
 is_on = True
-while is_on:
-    
-    print("What would you like? ")
-    drink_options = menu.get_items()
-    choice = input(f"Enter your choice from {drink_options}: ").lower()
 
-    drink = menu.find_drink(choice) # vaise too ye find_drink() method hai object return krta hai lets see what happens!!
-    if drink: # "off" is being treated like a drink name before you check for "off". A BUG that's why it prints "Sorry that item is not available." when off
-        # checking availability
-        if coffee_maker.is_resource_sufficient(drink):
-            # process coins
-            if money_machine.make_payment(drink.cost):
-                # make coffee
-                coffee_maker.make_coffee(drink)
-    # 🚨Command keywords (off, report) must be handled BEFORE business logic (find_drink).
-    elif choice == "report": # A BUG that's why it prints "Sorry that item is not available." when report
-       coffee_maker.report()
-       money_machine.report()
-    elif choice == "off": # A BUG that's why it prints "Sorry that item is not available." when off
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
         is_on = False
-    else: # no need i guess it already prints the item is not available (but anyways make it stay!!)
-        print("Invalid Choice")
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        
+        if drink is not None: # 📝✔️🟢
+            if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+# a bug when i type any invalid entry👇 -> correction point added 📝✔️🟢
+# 🔥 Root cause (very important)
+# find_drink() prints an error, but still returns None, and the main code doesn’t stop execution.
+
+# So:
+
+# Error message is printed ✔️
+# Program continues ❌
+# None is treated like a MenuItem
+# Program crashes 💥
