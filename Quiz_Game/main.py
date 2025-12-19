eb9bef2ce8026675a4a13fab7a7420d9182d1786
Nandini <nandinigoyal1981@gmail.com>
2025-12-19 22:23:13 +0530

    Fetch quiz questions from external API
    Replaced static question data with API call to fetch questions.

--- Quiz_Game/main.py
+++ Quiz_Game/main.py
@@ -1,18 +1,63 @@
+'''requests is a third-party Python library, which we import as a module, and it 
+provides functions like get() that return Response objects.
+all doubts 🤔 👇'''
+import requests
 from question_model import Question
-from data import question_data
 from quiz_brain import QuizBrain
 
+url = "https://opentdb.com/api.php?amount=10&category=18&type=boolean"
+response = requests.get(url)
+data = response.json()
+
 question_bank = []
-for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
 
-quiz = QuizBrain(question_bank)
+for q in data["results"]:
+    question = Question(q["question"], q["correct_answer"])
+    question_bank.append(question)
 
-while quiz.still_has_questions():
-    quiz.next_question()
 
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+quizbrain = QuizBrain(question_bank) 
+while quizbrain.still_has_questions(): 
+    quizbrain.next_question()
+
+print(f"You've completed the quiz,\nYour Final Score was: {quizbrain.score}/{len(question_bank)}")
+
+''' WORKING FLOW:
+Internet
+   ↓
+GET request
+   ↓
+JSON response
+   ↓
+Python dictionary
+   ↓
+Question objects
+   ↓
+QuizBrain
+
+👉⌚add info:
+requests → library (big)
+
+requests → module (what you import)
+
+Response → class inside it
+
+get() → function you call
+
+👉⌚que as doubt and ans:
+Q🤔: What does .json() do?
+
+It converts JSON response data into a Python dictionary.
+
+Q🤔: Difference between JSON and dictionary?
+
+JSON is a text-based data format, while a dictionary is a Python data structure.
+
+Q🤔: What is requests?
+
+requests is a Python module used to send HTTP requests.
+
+Q🤔: What is response?
+
+response is an object returned by requests.get() containing server response data.
+'''
